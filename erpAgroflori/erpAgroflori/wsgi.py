6a9ab41323a29e7af271f36b1fda7504530c36e4
Laura Delgadillo <delgadilloarterolaura@gmail.com>
2023-12-18 11:37:27 -0400

    Revert "develop Modified files for deploying the application on Azure."
    This reverts commit aa192009ec2da306ff4205449e749f9a93e8989d.

--- erpAgroflori/erpAgroflori/wsgi.py
+++ erpAgroflori/erpAgroflori/wsgi.py
@@ -11,8 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-settings_module = 'erpAgroflori.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'erpAgroflori.settings'
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'erpAgroflori.settings')
 
 application = get_wsgi_application()
